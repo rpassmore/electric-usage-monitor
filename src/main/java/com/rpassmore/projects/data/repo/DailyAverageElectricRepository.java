dfa13f8ea92294837bf6709418a9109f3286da0e
rob passmore <robert_passmore@yahoo.co.uk>
2016-10-27 11:25:24 +0100

    Alow access from different origins
    Add crossorigin annotation

--- src/main/java/com/rpassmore/projects/data/repo/DailyAverageElectricRepository.java
+++ src/main/java/com/rpassmore/projects/data/repo/DailyAverageElectricRepository.java
@@ -3,10 +3,12 @@ package com.rpassmore.projects.data.repo;
 import com.rpassmore.projects.data.entity.DailyAverageElectric;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by rob on 06/08/16.
  */
 @RepositoryRestResource(collectionResourceRel = "dailyreadings", path = "dailyreadings")
+@CrossOrigin
 public interface DailyAverageElectricRepository extends JpaRepository<DailyAverageElectric, Long> {
 }
--- src/main/java/com/rpassmore/projects/data/repo/ElectricReadingRepository.java
+++ src/main/java/com/rpassmore/projects/data/repo/ElectricReadingRepository.java
@@ -4,6 +4,7 @@ import com.rpassmore.projects.data.entity.ElectricReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  */
 
 @RepositoryRestResource(collectionResourceRel = "readings", path = "readings")
+@CrossOrigin
 public interface ElectricReadingRepository extends JpaRepository<ElectricReading, Long> {
   List<ElectricReading> findByReadingDateBetween(LocalDateTime start, LocalDateTime end);
 
