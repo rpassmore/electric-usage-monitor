b4fca5316f10683e1e477107051b9eb866bbc750
rpassmore <rpassmore@gmail.com>
2016-12-18 12:59:57 +0000

    dockerise (#6)
    * Created docker file
    
    Docker file for rpi with java 8
    
    * Add support for docker
    
    * Alow access from different origins
    
    Add crossorigin annotation
    
    * Fix error in previous comit
    
    * Enable CORS via a CORSFilter component
    
    Needed until Spring data rest supports the @AllowOrigin annotation
    
    * Make SpringDataRest return plain JSON instead of HATEOAS
    
    * Add commands reminder
    
    * Add futher services needed by client
    
    * Allow searching from clients

--- src/main/java/com/rpassmore/projects/data/repo/DailyAverageElectricRepository.java
+++ src/main/java/com/rpassmore/projects/data/repo/DailyAverageElectricRepository.java
@@ -1,9 +1,15 @@
 package com.rpassmore.projects.data.repo;
 
 import com.rpassmore.projects.data.entity.DailyAverageElectric;
+import com.rpassmore.projects.data.entity.ElectricReading;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by rob on 06/08/16.
@@ -11,4 +17,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "dailyreadings", path = "dailyreadings")
 @CrossOrigin
 public interface DailyAverageElectricRepository extends JpaRepository<DailyAverageElectric, Long> {
+  List<DailyAverageElectric> findByDateBetween(@Param("start")
+                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+                                          @Param("end")
+                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end);
 }
